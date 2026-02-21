e1b8261139d897e256ebb8b8e536d2a66df4d658
Sukaato <sukaato.dev@proton.me>
2026-02-21 13:30:57 +0100

    chore: update Cargo.lock and lib.rs exports

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,16 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 mod commands;
-mod dataset;
+mod config;
 mod db;
+mod services;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![
-            commands::greet,
+            commands::logger::log,
+            commands::config::get_config,
             commands::database::init_db,
             commands::collection::add_to_collection,
             commands::collection::get_collection,
